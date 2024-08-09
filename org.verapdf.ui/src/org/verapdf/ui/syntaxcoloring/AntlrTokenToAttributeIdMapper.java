e59f4af87ddd0a8fe3a04a077a90989a6eb837be
Maxim <maxplusim@mail.ru>
2024-08-09 19:40:00 +0300

    Update license headers

--- org.verapdf.ui/src/org/verapdf/ui/syntaxcoloring/AntlrTokenToAttributeIdMapper.java
+++ org.verapdf.ui/src/org/verapdf/ui/syntaxcoloring/AntlrTokenToAttributeIdMapper.java
@@ -1,3 +1,23 @@
+/**
+ * This file is part of VERA PDF UI, a module of the veraPDF project.
+ * Copyright (c) 2015-2024, veraPDF Consortium <info@verapdf.org>
+ * All rights reserved.
+ *
+ * VERA PDF UI is free software: you can redistribute it and/or modify
+ * it under the terms of either:
+ *
+ * The GNU General public license GPLv3+.
+ * You should have received a copy of the GNU General Public License
+ * along with VERA PDF UI as the LICENSE.GPL file in the root of the source
+ * tree.  If not, see http://www.gnu.org/licenses/ or
+ * https://www.gnu.org/licenses/gpl-3.0.en.html.
+ *
+ * The Mozilla Public License MPLv2+.
+ * You should have received a copy of the Mozilla Public License along with
+ * VERA PDF UI as the LICENSE.MPL file in the root of the source tree.
+ * If a copy of the MPL was not distributed with this file, you can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
 package org.verapdf.ui.syntaxcoloring;
 
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultAntlrTokenToAttributeIdMapper;
